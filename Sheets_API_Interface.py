dd609c45ba6e29ab1aa3942ea9458c0d7a9a71c0
Aakarsh Jain <jainaakarsh09@gmail.com>
2022-10-31 03:44:05 +0530

    Trying to debug

--- Sheets_API_Interface.py
+++ Sheets_API_Interface.py
@@ -26,7 +26,12 @@ USERNAME_TO_INDEX:dict[str:int] = json.load(open("usernames.json", "r"))
 #getting and loading the sheet
 SCOPE = ["https://spreadsheets.google.com/feeds",'https://www.googleapis.com/auth/spreadsheets',"https://www.googleapis.com/auth/drive.file","https://www.googleapis.com/auth/drive"]
 # credentials = ServiceAccountCredentials.from_json_keyfile_name("creds.json", SCOPE)
-credentials = ServiceAccountCredentials.from_json_keyfile_dict(json.load(os.environ["GOOGLE_API_CRED"]), SCOPE)
+try:
+    env_creds = os.environ["GOOGLE_API_CRED"]
+    print(type(env_creds))
+    credentials = ServiceAccountCredentials.from_json_keyfile_dict(json.load(), SCOPE)
+except Exception:
+    print(Exception)
 client = gspread.authorize(credentials)
 
 # opening the entire document
