659ec0176c6b0c8fde561afb240d0b09c9044284
Anirudh S. Kumar <anirudh.skumar.03@gmail.com>
2023-01-11 22:29:51 +0530

    migrated from firefox to edge

--- scripts/LC_Data_Scraper.py
+++ scripts/LC_Data_Scraper.py
@@ -1,11 +1,12 @@
 from selenium import webdriver
-from selenium.webdriver.firefox.options import Options
-from selenium.webdriver.firefox.service import Service
-from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
+from selenium.webdriver.edge.options import Options as EdgeOptions
+from datetime import datetime, timedelta
+from webdriver_manager.microsoft import EdgeChromiumDriverManager
+from selenium.webdriver.edge.service import Service as EdgeService
 import selenium.common.exceptions
 import time
 import parsedatetime as pdt
-from datetime import datetime, timedelta
+
 
 TIME_DELTA_HOUR:int = 2
 TIME_DELTA_DAY:int = 1
@@ -13,9 +14,7 @@ LOG_PATH:str = "./geckodriver.log"
 EXE_PATH:str = "./geckodriver.exe"
 
 cal = pdt.Calendar()
-options = Options()
-firefox_options = Options()
-firefox_binary = FirefoxBinary()
+edge_options = EdgeOptions()
 options = [
     "--headless",
     "--disable-gpu",
@@ -27,9 +26,9 @@ options = [
 ]
 
 for option in options:
-    firefox_options.add_argument(option)
+    edge_options.add_argument(option)
 
-browser = webdriver.Firefox(options=firefox_options)  
+browser = webdriver.Edge(service=EdgeService(EdgeChromiumDriverManager().install()), options=edge_options)
 
 # getting the questions done in the past 24 hours
 def get_questions(username: str) -> list[str]:
@@ -59,6 +58,7 @@ def get_questions(username: str) -> list[str]:
         if (datetime.now() - datetime_obj < cutoff):
             if Child[0].text:
                 rval.append(Child[0].text)
+
     return rval
 
 
@@ -68,5 +68,3 @@ if __name__ == "__main__":
     print(get_questions("aakarsh_11235"))
     # print(get_questions("vartika_7"))
     browser.quit()
-
-# /html/body/div[1]/div/div[2]/div/div[2]/div[3]/div/div/div[2]
