a8618526489367d891666c7fd191edb03704f743
Anirudh S. Kumar <61222180+Anirudh-S-Kumar@users.noreply.github.com>
2023-01-11 21:37:43 +0530

    Update LC_Data_Scraper.py

--- scripts/LC_Data_Scraper.py
+++ scripts/LC_Data_Scraper.py
@@ -8,7 +8,7 @@ import parsedatetime as pdt
 from datetime import datetime, timedelta
 
 TIME_DELTA_HOUR:int = 2
-TIME_DELTA_DAY:int = 0
+TIME_DELTA_DAY:int = 1
 LOG_PATH:str = "./geckodriver.log"
 EXE_PATH:str = "./geckodriver.exe"
 
